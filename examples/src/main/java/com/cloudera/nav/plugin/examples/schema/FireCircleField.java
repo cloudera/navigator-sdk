fe3297c7da237ec78f31844806ad9edf7008a0bb
Chang She <chang@cloudera.com>
2015-06-29 11:06:33 -0700

    [example][model] Add schema related models and example
    This commit adds Dataset and DatasetField models to be used for
    creating dataset schemas and linking them to the right data
    containers. A new example examples/schema is added that shows
    how custom dataset schemas can be reported to Navigator using the
    plugin

---
+++ examples/src/main/java/com/cloudera/nav/plugin/examples/schema/FireCircleField.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 Cloudera, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudera.nav.plugin.examples.schema;
+
+import com.cloudera.nav.plugin.model.SourceType;
+import com.cloudera.nav.plugin.model.annotations.MClass;
+import com.cloudera.nav.plugin.model.entities.DatasetField;
+
+@MClass
+public class FireCircleField extends DatasetField {
+
+  public FireCircleField() {
+    super();
+    setSourceType(SourceType.PLUGIN);
+    setNamespace("FireCircle");
+  }
+
+  public FireCircleField(String name, String type) {
+    this();
+    setName(name);
+    setDataType(type);
+  }
+}
