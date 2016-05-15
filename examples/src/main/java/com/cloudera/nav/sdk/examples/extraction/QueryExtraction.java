083dac91c6f186b111736747d3e9bc17a6d3b263
Naren Koneru <naren@cloudera.com>
2016-05-15 14:26:22 -0700

    [Extraction] Code to extract queries that can be an input for Optimizer and Sigma.
    It produces two output files one for the optimizer and one for sigma.

--- examples/src/main/java/com/cloudera/nav/sdk/examples/extraction/QueryExtraction.java
+++ examples/src/main/java/com/cloudera/nav/sdk/examples/extraction/QueryExtraction.java
@@ -7,8 +7,8 @@ import com.cloudera.nav.sdk.client.MetadataResultIterator;
 import com.cloudera.nav.sdk.client.MetadataResultSet;
 import com.cloudera.nav.sdk.client.NavApiCient;
 import com.google.common.base.Joiner;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.opencsv.CSVWriter;
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 import org.apache.log4j.Level;
@@ -29,7 +28,6 @@ import org.slf4j.LoggerFactory;
 public class QueryExtraction {
   private static final Logger LOG =
       LoggerFactory.getLogger(QueryExtraction.class);
-  private static final Set SOURCE_TYPES = Sets.newHashSet();
 
   private static Integer limit = 500000;
 
@@ -40,58 +38,32 @@ public class QueryExtraction {
     String principal;
     private Instant minStartTime;
     private Instant maxEndTime;
+    private List<String> mrStartTimes = Lists.newArrayList();
+    private List<String> mrEndTimes = Lists.newArrayList();
+    int numStages;
 
-    public OperationExecution(MetadataExtractor extractor, Map<String, Object> obj) {
+    public OperationExecution(Map<String, Object> obj) {
       identity = (String) obj.get("identity");
       Object started = obj.get("started");
       if (started != null) {
         startTime = Instant.parse((String) obj.get("started"));
       }
       principal = (String) obj.get("principal");
-
-//      Instant minStartTime = null, maxEndTime = null;
-//      String relationsQuery = "type:LOGICAL_PHYSICAL AND endpoint1Ids:" + identity;
-//
-//      MetadataResultSet resultSet = extractor.extractMetadata(null, null,
-//          null, relationsQuery);
-//      IncrementalExtractIterator iterator = resultSet.getRelations().iterator();
-//      while(iterator.hasNext()) {
-//        obj = iterator.next();
-//
-//        Map<String, Object> instances = (Map<String, Object>) obj.get("physical");
-//        List<String> physicalIds = (List<String>) instances.get("entityIds");
-//
-//        StringBuilder query = new StringBuilder("(");
-//        for(String id : physicalIds) {
-//          query.append("identity:" + id + " OR ");
-//        }
-//        query.append("identity:dummy)");
-//
-//        MetadataResultSet physicalResultSet = extractor.extractMetadata(null,
-//            null, query.toString(), null);
-//        IncrementalExtractIterator physicalEntities = physicalResultSet
-//            .getEntities().iterator();
-//        while(physicalEntities.hasNext()) {
-//          Map<String, Object> mrEntity = physicalEntities.next();
-//          minStartTime = getMinTime(minStartTime, mrEntity.get("started"));
-//          maxEndTime = getMaxTime(maxEndTime, mrEntity.get("ended"));
-//        }
-//      }
-//
-//      if (minStartTime != null && maxEndTime != null) {
-//        duration = maxEndTime.minus(minStartTime.getMillis()).getMillis();
-//      }
     }
 
     public void addMrJob(Map<String, Object> mrJobExec) {
       minStartTime = getMinTime(minStartTime, mrJobExec.get("started"));
       maxEndTime = getMaxTime(maxEndTime, mrJobExec.get("ended"));
+      this.mrStartTimes.add((String) mrJobExec.get("started"));
+      this.mrEndTimes.add((String) mrJobExec.get("ended"));
+      numStages++;
     }
 
     public void computeDuration() {
       if (minStartTime != null && maxEndTime != null) {
         duration = maxEndTime.minus(minStartTime.getMillis()).getMillis();
       }
+
     }
   }
 
@@ -101,53 +73,120 @@ public class QueryExtraction {
     Instant lastStarted;
     String lastPrincipal;
 
-    int totalExecutions;
-
     TreeSet<Long> durations = Sets.newTreeSet();
+    TreeSet<Instant> startTimes = Sets.newTreeSet();
+    TreeSet<String> users = Sets.newTreeSet();
 
     long minDuration;
     long maxDuration;
     long averageDuration;
     long medianDuration;
+    List<OperationExecution> executions = Lists.newArrayList();
+    private String minStartTime;
+    private String maxStartTime;
+    private String userString;
+    private int maxStages;
 
     public Operation(String opId) {
       this.identity = opId;
 
     }
 
-    public void addOperationInstances(List<String> execIds, Map<String,
-        OperationExecution> opExecs) {
-      totalExecutions += execIds.size();
-
+    public void addOperationInstances1(List<String> execIds, Map<String,
+        OperationExecution> allOpExecs) {
 
       for(String execId : execIds) {
-        OperationExecution operationExecution = opExecs.get(execId);
+        OperationExecution operationExecution = allOpExecs.get(execId);
         if (lastStarted == null || lastStarted.isBefore(operationExecution.startTime)) {
           lastStarted = operationExecution.startTime;
           lastPrincipal = operationExecution.principal;
         }
         durations.add(operationExecution.duration);
         averageDuration += operationExecution.duration;
+        executions.add(operationExecution);
       }
+    }
+
+    public void addOperationInstances(List<String> execIds, Map<String,
+        OperationExecution> allOpExecs) {
+      for(String execId : execIds) {
+        OperationExecution operationExecution = allOpExecs.get(execId);
+        executions.add(operationExecution);
+      }
+    }
+
+    public void computeDurations() {
+      for(OperationExecution operationExecution : executions) {
+        durations.add(operationExecution.duration);
+        averageDuration += operationExecution.duration;
+
+        if (operationExecution.startTime != null) {
+          startTimes.add(operationExecution.startTime);
+        }
+        if (operationExecution.principal != null) {
+          users.add(operationExecution.principal);
+        }
+        maxStages =
+            maxStages > operationExecution.numStages ? maxStages : operationExecution.numStages;
+      }
+
       minDuration = durations.first();
       maxDuration = durations.last();
       medianDuration = Iterables.get(durations, (durations.size() - 1) / 2);
-      averageDuration = averageDuration/execIds.size();
+      averageDuration = averageDuration/executions.size();
+
+      minStartTime = startTimes.isEmpty() ? "" : startTimes.first().toString();
+      maxStartTime = startTimes.isEmpty() ? "" : startTimes.last().toString();
+      userString = Joiner.on(",").skipNulls().join(users);
     }
 
-    public void write(int index, CSVWriter csvWriter) {
+    public void writeOptimizerFormat(int index, CSVWriter csvWriter) {
       String queryType = getQueryType(escapeQueryText(queryText));
 
       csvWriter.writeNext(new String[] {Integer.toString(index++),
           identity,
-          Integer.toString(totalExecutions),
+          Integer.toString(executions.size()),
           Long.toString(minDuration),Long.toString(maxDuration),
           Long.toString(averageDuration),Long.toString(medianDuration),
-          lastPrincipal, lastStarted != null ? lastStarted.toString() : "", queryType, queryText});
+          minStartTime, maxStartTime, Integer.toString(maxStages),
+          userString, queryType, queryText});
+    }
 
+    public void writeSigmaFormat(int index, CSVWriter csvWriter) {
+      for (OperationExecution oe : executions) {
+        for (int i = 0; i < oe.mrEndTimes.size(); i++) {
+          if (oe.mrStartTimes.get(i) == null ||
+              oe.mrEndTimes.get(i) == null) {
+            continue;
+          }
+
+          csvWriter.writeNext(new String[] {
+              oe.identity,
+              Long.toString(Instant.parse(oe.mrStartTimes.get(i)).getMillis()),
+              Long.toString(Instant.parse(oe.mrEndTimes.get(i)).getMillis()),
+              escapeQueryText(queryText)
+          });
+        }
+      }
+    }
+
+    public void write(int i, CSVWriter sigmaCSVWriter, CSVWriter
+        optimizerCsvWriter) {
+      writeSigmaFormat(i, sigmaCSVWriter);
+      writeOptimizerFormat(i, optimizerCsvWriter);
+    }
+
+    public static void writeHeaders(CSVWriter sigmaCSVWriter,
+                                    CSVWriter optimizerCsvWriter) {
+      sigmaCSVWriter.writeNext(new String[] {
+          "QueryExecIdentity, mrStartTime, mrEndTime, query"});
+      optimizerCsvWriter.writeNext(new String[] {
+          "index, QueryIdentity, totalExecutions, minDuration, maxDuration, averageDuration, medianDuration," +
+              "users, minStartTime, maxStartTime, queryType"});
     }
   }
 
+  @SuppressWarnings("unchecked")
   public static  void main(String[] args) throws IOException {
     LogManager.getRootLogger().setLevel(Level.INFO);
 
@@ -159,30 +198,11 @@ public class QueryExtraction {
     NavApiCient client = new NavApiCient(config);
     MetadataExtractor extractor = new MetadataExtractor(client, limit);
 
-    // Whats my starting marker?
-//    String marker;
-//    try {
-//      String markerReadPath = args[1];
-//      File markerFile = new File(markerReadPath);
-//      FileReader fr = new FileReader(markerFile);
-//      BufferedReader markerReader = new BufferedReader(fr);
-//      marker = markerReader.readLine();
-//      markerReader.close();
-//      fr.close();
-//    } catch (IOException e) {
-//      throw Throwables.propagate(e);
-//    } catch (ArrayIndexOutOfBoundsException e){
-//      marker=null;
-//    }
-
-    String outputFile = args[1];
-    PrintWriter outputWriter = new PrintWriter(outputFile, "UTF-8");
-    outputWriter.println(
-        "identity, duration, queryType, query");
-
-
     // Collect all the operation executions.
-    String entityQuery = "sourceType:IMPALA AND (type:operation_execution)";
+    String entityQuery = "sourceType:HIVE AND (type:operation_execution)";
+
+//    String entityQuery =
+//        "+(sourceType:HIVE AND (type:operation_execution)) +(+sourceType:hive +started:[NOW/DAY-5DAYS TO NOW/DAY+1DAY])";
 
     // GEt all the op execs and their time, user
     MetadataResultSet resultSet = null;
@@ -194,7 +214,7 @@ public class QueryExtraction {
     while(iterator.hasNext()) {
       Map<String, Object> obj = iterator.next();
       OperationExecution operationExecution =
-          new OperationExecution(extractor, obj);
+          new OperationExecution(obj);
       opExecs.put(operationExecution.identity, operationExecution);
 
       if (opExecs.size()%1000 == 0) {
@@ -205,7 +225,7 @@ public class QueryExtraction {
     LOG.info("Obtained {} operation executions", opExecs.size());
 
     // Collect MR operations so that we can collect the elapsed time.
-    //collectElapsedTimes(extractor, opExecs);
+    collectElapsedTimes(extractor, opExecs);
 
     // Collect operation Ids.
     Map<String, Operation> operations = Maps.newHashMap();
@@ -239,10 +259,9 @@ public class QueryExtraction {
       }
     }
 
-    // Collect operation details like queryText, etc and write it out.
-    CSVWriter csvWriter =
-        new CSVWriter(outputWriter, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, "@@@@@");
-
+    for (Operation operation : operations.values()) {
+      operation.computeDurations();
+    }
 
     index = 0;
     LOG.info("Obtained {} operations", operations.size());
@@ -261,31 +280,34 @@ public class QueryExtraction {
 
         Operation operation = operations.get(identity);
         operation.queryText = queryText == null ? "" : queryText;
-        operation.write(index++, csvWriter);
       }
+    }
+
+    String sigmaOutputFile = args[1];
+    String optimizerOutputFile = args[2];
+
+    PrintWriter sigmaWriter = new PrintWriter(sigmaOutputFile, "UTF-8");
+    CSVWriter sigmaCSVWriter =
+        new CSVWriter(sigmaWriter, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, "\n");
+
+    PrintWriter optimizerWriter = new PrintWriter(optimizerOutputFile, "UTF-8");
+    CSVWriter optimizerCsvWriter =
+        new CSVWriter(optimizerWriter, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, "@@@@@");
+        //new CSVWriter(optimizerWriter, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.DEFAULT_QUOTE_CHARACTER, "\n");
+
+    Operation.writeHeaders(sigmaCSVWriter, optimizerCsvWriter);
+
+    for(Operation operation : operations.values()) {
+      operation.write(index++, sigmaCSVWriter, optimizerCsvWriter);
     }
 
     LOG.info("Processed {} operations", index);
 
-    String nextMarker = "";
-    //String nextMarker = getAllRelationships(extractor, marker);
-    //String nextMarker = getSelectiveRelationships(extractor, marker, outputWriter);
-
-    csvWriter.close();
-
-    // Save the last marker so that it can be used for the next harvest.
-    try {
-      String markerWritePath = args[3];
-      PrintWriter markerWriter = new PrintWriter(markerWritePath, "UTF-8");
-      markerWriter.println(nextMarker);
-      markerWriter.close();
-    } catch(IOException e) {
-      throw Throwables.propagate(e);
-    } catch (ArrayIndexOutOfBoundsException e){
-      LOG.error("Please specify a file to save next marker");
-    }
+    sigmaCSVWriter.close();
+    optimizerCsvWriter.close();
   }
 
+  @SuppressWarnings({ "unused", "unchecked" })
   private static void collectElapsedTimes(MetadataExtractor extractor,
                                           Map<String, OperationExecution> opExecs) {
     LOG.info("Processing elapsed times");
@@ -376,7 +398,7 @@ public class QueryExtraction {
 
   private static String escapeQueryText(String queryText) {
     queryText = queryText.replaceAll("\"", "\\\"");
-    return queryText.replaceAll("\r\n", "    ");
+    return queryText.replaceAll("\n", " ");
   }
 
   private static String getQueryType(String queryText) {
