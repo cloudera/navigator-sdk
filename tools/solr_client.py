8ff1bb5459de22dfdf67c39108d8c40fe5e55235
Chang She <chang@cloudera.com>
2016-05-27 16:24:05 -0700

    [tools] Port Solr analyzer tool from Navigator repository
    We make the analyzer tool available from the navigator-sdk library for
    easier distribution and access. This tool analyzes Navigator's Solr
    index to produce stats that reveal customer usage patterns.

---
+++ tools/solr_client.py
@@ -0,0 +1,308 @@
+"""
+A light-weight python client for Solr servers tested on 4.10
+"""
+import pandas as pd
+import requests
+
+class SolrServer(object):
+    """
+    Encapsulates connection to a given Solr server
+    """
+    def __init__(self, host, port, user, pwd, debug=False):
+        """
+        Parameters
+        ----------
+        host: str
+          e.g., vc0330.halxg.cloudera.com
+        port: int
+          e.g., 7512
+        user: str
+          user name
+        pwd: str
+          password
+        """
+        self.host = host
+        self.port = int(port)
+        self.user = user
+        self.pwd = pwd
+        self._debug = debug
+        self._session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(max_retries=10)
+        self._session.mount('http://', adapter)
+
+    @property
+    def url(self):
+        return 'http://{}:{}/solr'.format(self.host, self.port)
+
+    def post(self, url, data=None):
+        if self._debug:
+            print('Query: ' + str(data))
+        return self._session.post(url, auth=(self.user, self.pwd), data=data)
+
+    def get(self, url, params=None):
+        if self._debug:
+            print('Query: ' + str(params))
+        return self._session.get(url, auth=(self.user, self.pwd),
+                                  params=params)
+
+    def get_core(self, core_name):
+        return SolrCore(self, core_name)
+
+    def core_admin_status(self, name=None):
+        """
+        Check core admin status and return results in json.
+        If name is unspecified, returns all cores.
+        """
+        params={'action':'STATUS', 'wt':'json'}
+        if name is not None:
+            params['core'] = name
+        return self.get(self.url + '/admin/cores', params=params).json()
+
+
+
+class SolrCore(object):
+    """
+    Client to execute queries against a core on a given Solr server.
+    """
+
+    def __init__(self, server, core):
+        """
+        Parameters
+        ----------
+        server: SolrServer
+        core: str
+          name of the core 'nav_elements' or 'nav_relations'
+        """
+        self.server = server
+        self.core = core
+        self._schema = None
+
+    @property
+    def url(self):
+        """
+        Nav does not have TLS auth as of 2.6.0 so always http
+        """
+        return '{}/{}'.format(self.server.url, self.core)
+
+    @property
+    def schema(self):
+        if self._schema is None:
+            self._schema = SolrSchema(self)
+        return self._schema
+
+    def query(self, q='*:*', fq='', sort='', wt='json', rows=0, indent='true',
+              params=None):
+        """
+        We're not using ** magic here so we can do parameters like 'facet.pivot'
+        """
+        q_params = {
+            'q': q,
+            'fq': fq,
+            'rows': rows,
+            'indent': indent,
+            'wt': wt,
+            'sort': sort
+        }
+        if params is not None:
+            q_params.update(params)
+        return self.server.post('{}/select'.format(self.url),
+                               data=q_params).json()
+
+    def get_docs(self, q='*:*', fq='', sort='', wt='json', rows=None,
+                 indent='true', batch_size=100000, params=None):
+        """
+        Generator
+        """
+        if params is None:
+            params = {}
+        _docs = lambda resp: resp.get('response').get('docs')
+        if rows is not None:
+            for d in _docs(self.query(q=q, fq=fq, sort=sort, wt=wt, rows=rows,
+                                      indent='true', params=params)):
+                yield d
+        else:
+            assert sort is not None and len(sort) > 0
+            cursor = '*'
+            while True:
+                params['cursorMark'] = cursor
+                batch = self.query(q=q, fq=fq, sort=sort, wt=wt, rows=batch_size,
+                                   indent='true', params=params)
+                for d in _docs(batch):
+                    yield d
+                cursor = batch['nextCursorMark']
+                if params['cursorMark'] == cursor:
+                    break
+
+    def get_count(self, q='*:*', fq='', params=None):
+        if params is None:
+            params = {}
+        resp = self.query(q=q, fq=fq, rows=0, params=params)
+        return resp['response']['numFound']
+
+    def pivot(self, fields, fq=''):
+        """
+        Execute a facet pivot query and return results as a DataFrame
+
+        e.g, solr_core.pivot(['sourceType', 'type']) returns a DataFrame like:
+
+                                        count
+        sourceType	type
+        hdfs		                    22660220
+                    file	            17846879
+                    directory	        3880049
+                    source	            1
+        hive		                    5395137
+                    field	            2553551
+                    sub_operation	    1827283
+                    partition	        599414
+                    operation_execution	258680
+                    operation	        97738
+                    table	            52900
+                    view	            3310
+                    database	        2260
+                    source              1
+        ... ...
+
+        Parameters
+        ----------
+        fields: list-like
+          facet.pivot field names
+        fq: str, default ''
+          Filter query
+        """
+        json_resp = self.query(fq=fq, params={'facet':'true',
+                                              'facet.pivot':','.join(fields)})
+        pivot_data = json_resp['facet_counts']['facet_pivot'][','.join(fields)]
+        levels = len(fields)
+        cols = list(fields)
+        cols.append('count')
+        flattened = _flatten(pivot_data, [], levels)
+        return pd.DataFrame(flattened, columns=cols).set_index(fields)
+
+    def stats(self, fields, fq='', stats=None):
+        """
+        Execute a Solr stats query and return results as a DataFrame
+
+        e.g., solr_core.stats('size', stats=['max', 'sum', 'mean', 'stddev']
+        Returns a DataFrame like:
+
+                size
+        max	    3.64468e+12
+        sum	    3.96866e+14
+        mean	2.22373e+07
+        stddev	2.00506e+09
+
+        Parameters
+        ----------
+        fields: str or list
+          Fields to get stats on
+        fq: str, default ''
+          Filter query
+        stats: list, optional
+          List of stats names, or all stats if unspecified
+        """
+        json_resp = self.query(fq=fq, params={'stats':'true',
+                                              'stats.field':fields})
+        stats_data = json_resp['stats']['stats_fields']
+        df = pd.DataFrame.from_dict(stats_data)
+        if stats is not None:
+            df = df.reindex(stats)
+        df.index.name = 'stats'
+        return df
+
+    def facet_field(self, fields, fq='', limit=100, mincount=1):
+        json_resp = self.query(fq=fq, params={'facet':'true',
+                                              'facet.field':fields,
+                                              'facet.limit':limit,
+                                              'facet.mincount':mincount})
+        raw_data = json_resp['facet_counts']['facet_fields']
+        reshaped = {}
+        for field_name, field_data in raw_data.iteritems():
+            labels = []
+            values = []
+            for i in range(int(len(field_data) / 2)):
+                labels.append(field_data[i * 2])
+                values.append(field_data[i * 2 +1])
+            labels = pd.Index(labels, name=field_name)
+            reshaped[field_name] = pd.Series(values, labels, name='Count')
+        return pd.DataFrame(reshaped)
+
+    def facet_query(self, queries, fq=''):
+        json_resp = self.query(fq=fq, params={'facet':'true',
+                                              'facet.query':queries})
+        data = json_resp['facet_counts']['facet_queries']
+        return pd.Series(data)
+
+    def facet_range(self, field, start, end, gap, fq=''):
+        json_resp = self.query(fq=fq, params={'facet': 'true',
+                                              'facet.range': field,
+                                              'facet.range.start': start,
+                                              'facet.range.end': end,
+                                              'facet.range.gap': gap})
+        data = json_resp['facet_counts']['facet_ranges'][field]['counts']
+        labels = []
+        values = []
+        for i in range(int(len(data) / 2)):
+            labels.append(data[i * 2])
+            values.append(data[i * 2 +1])
+        return pd.Series(values, pd.to_datetime(labels))
+
+
+
+class SolrSchema(object):
+
+    def __init__(self, core):
+        self._core = core
+        schema = core.server.get('{}/schema'.format(core.url)).json()
+        for f in schema['schema']['fields']:
+            field = SolrField(self, f['name'], f['type'], f['multiValued'],
+                              f['indexed'], f['stored'])
+            setattr(self, field.name, field)
+
+
+
+class SolrField(object):
+
+    def __init__(self, schema, name, field_type, multi, indexed, stored):
+        self.schema = schema
+        self.name = name
+        self.field_type = field_type
+        self.multi = multi
+        self.indexed = indexed
+        self.stored = stored
+
+    def __repr__(self):
+        return self.name
+
+
+
+def frange(field, l, u, incl=True, incu=False, key=None):
+    incl = str(incl).lower()
+    incu = str(incu).lower()
+    key = '' if key is None else ' key="' + key + '"'
+    query = '{!frange'
+    if l is not None:
+        query += ' l={l} incl={incl}'.format(**locals())
+    if u is not None:
+        query += ' u={u} incu={incu}'.format(**locals())
+    query += key + '}' + field
+    return query
+
+def _flatten(lst, prefixes, tot_levels):
+    """
+    Convert the response from facet pivot API into a flattened list
+    """
+    rs = []
+    for rec in lst:
+        row = list(prefixes)
+        row.append(rec['value'])
+        diff = tot_levels - len(row)
+        if diff > 0:
+            row.extend([' '] * diff) # use space so Excel doesn't merge cells
+        row.append(rec['count'])
+        rs.append(row)
+        if 'pivot' in rec:
+            next_pref = list(prefixes)
+            next_pref.append(rec['value'])
+            rs.extend(_flatten(rec['pivot'], next_pref, tot_levels))
+    return rs
---
+++ tools/solr_stats.py
@@ -0,0 +1,307 @@
+#! /usr/bin/env python
+"""
+Contains classes and functions to compute stats for Navigator Solr servers
+and compare across deployments
+
+Since this is focused on Navigator analysis as of 2.6.0,
+it assumes Solr 4.10 and may break against Solr 5+
+"""
+from solr_client import SolrServer, SolrCore, frange
+import pandas as pd
+import re
+
+USAGE = """
+Usage: solr_stats.py <config_file_path> <output_xlsx_path>"
+
+The config file should contain the details of all the Navigator deployments you
+wish to compare. Each deployment's config should be one a separate line in the
+format:
+
+    name,host,port,user,password
+
+e.g.,
+
+    customer1,foo.cloudera.com,1234,user,password
+    customer2,bar.cloudera.com,1234,user,password
+"""
+
+PATH_PATTEN = re.compile('(?:hdfs://[^/]*)(.*)')
+
+
+
+class NavSolrServer(SolrServer):
+    """
+    Navigator Solr server is a SolrServer with convenience properties
+    for 'nav_elements' and 'nav_relations', Navigator's 2 Solr cores.
+    """
+
+    @property
+    def nav_elements(self):
+        return self.get_core('nav_elements')
+
+    @property
+    def nav_relations(self):
+        return NavRelations(self, 'nav_relations')
+
+    def get_core(self, core):
+        if core == 'nav_relations':
+            return NavRelations(self, core)
+        elif core == 'nav_elements':
+            return NavCore(self, core)
+        return super(NavSolrServer, self).get_core(core)
+
+
+
+class NavCore(SolrCore):
+
+    def get_docs(self, q='*:*', fq='', sort=None, wt='json',
+                 rows=None, indent='true', batch_size=100000, params=None):
+        if rows is None and sort is None:
+            sort = 'identity asc'
+        return super(NavCore, self).get_docs(q, fq, sort, wt, rows, indent,
+                                             batch_size, params)
+
+    def find_by_id(self, ids, fl=None):
+        for part in partition(ids):
+            for r in self.get_docs(fq=terms(part, self.schema.identity),
+                                   params={'fl': fl}):
+                yield r
+
+
+
+def partition(lst, size=50*1024):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
+
+def terms(ids, field):
+    return '{{!terms f={}}}{}'.format(field, ','.join(ids))
+
+
+
+class NavRelations(NavCore):
+
+    def __init__(self, server, core):
+        super(NavRelations, self).__init__(server, core)
+        assert isinstance(server, NavSolrServer)
+
+    def get_ep1_ids(self, relation_query):
+        return self.get_endpoint_ids(relation_query, 'endpoint1Ids')
+
+    def get_ep2_ids(self, relation_query, limit=None):
+        return self.get_endpoint_ids(relation_query, 'endpoint2Ids')
+
+    def get_endpoint_ids(self, relation_query, endpoint):
+        ids = []
+        for d in self.get_docs(relation_query,
+                               params={'fl': ['identity', endpoint]}):
+            ids.extend(d[endpoint])
+        return ids
+
+
+
+class NavSolrAnalyzer(object):
+    """
+    Class to get server level summary stats across cores, HDFS stats,
+    and a breakdown of entity counts
+    """
+
+    def __init__(self, name, host, port, user, pwd):
+        self.name = name
+        self.server = NavSolrServer(host, port, user, pwd)
+        self.nav_elements = self.server.nav_elements
+
+    def summary_stats(self):
+        """
+        # docs and size for each core in the given SolrServer
+        """
+        resp = self.server.core_admin_status()
+        core_names = []
+        stats = ['indexHeapUsageBytes', 'numDocs', 'size']
+        data = []
+        for core, status in resp['status'].items():
+            core_names.append(core)
+            data.append([status['index'][field] for field in stats])
+        return pd.DataFrame(data, index=pd.Index(core_names, name='core'),
+                            columns=pd.Index(stats, name='stats')).T
+
+    def hdfs_stats(self):
+        """
+        Get HDFS file size summary statistics (non-deleted only)
+        """
+        return self.nav_elements.stats(
+            'size', fq='sourceType:HDFS AND type:FILE AND -deleted:true',
+            stats=['max', 'sum', 'mean', 'stddev'])
+
+    def count_breakdown(self):
+        """
+        Get entity counts by source type (HDFS, Yarn, etc) and entity type
+        (File, Operation, etc) for non-deleted entities only
+        """
+        return self.nav_elements.pivot(fields=['sourceType', 'type'],
+                                       fq='-deleted:true')
+
+    def deleted_stats(self, fq='sourceType:HDFS'):
+        fq += ('' if fq is None or fq == '' else ' AND ') + 'deleted:true'
+        delete_time = self.nav_elements.stats(
+            'deleteTime',
+            fq=fq + 'AND {!frange l=0 incl=false}deleteTime',
+            stats=['max', 'min'])
+        min_date = int(delete_time.ix['min', 0])
+        max_date = int(delete_time.ix['max', 0])
+        day_in_millis = 1000 * 60 * 60 * 24
+        break_points = [0, 1, 7, 30, 90, 365, 730]
+        labels = ['1 day', '1 week', '1 month', '3 months', '1 year', '2 years',
+                  'invalid deleteTime']
+        queries = []
+        for i in range(len(break_points)):
+            u = max_date - break_points[i] * day_in_millis
+            if i < len(break_points) - 1:
+                l = max(min_date, max_date - break_points[i+1] * day_in_millis)
+            else:
+                l = min_date
+            queries.append(self._make_deleteTime_query(l, u, label=labels[i]))
+            if l == min_date:
+                break
+        queries.append(self._make_deleteTime_query(None, min_date, incl=True,
+                                                   label='invalid deleteTime'))
+        rs = self.nav_elements.facet_query(queries, fq=fq)
+        rs = rs.reindex(labels).dropna().astype('int64')
+        rs.name = 'Deleted'
+        rs.index.name = 'Date Range'
+        return rs.to_frame()
+
+    def create_stats(self, start='NOW-1YEAR', end='NOW', gap='+1MONTH',
+                     fq='sourceType:HDFS'):
+        fq += (('' if fq is None or fq == '' else ' AND ') +
+               'created:[* TO *] AND -deleted:true')
+        ser = self.nav_elements.facet_range('created', start, end, gap, fq=fq)
+        ser.index.name = 'Date'
+        ser.name = 'Created'
+        return ser.to_frame()
+
+    def top_partitions(self, n=10):
+        fq = 'type:PARTITION AND sourceType:HIVE AND -deleted:true'
+        df = self.nav_elements.facet_field('parentPath', fq=fq,
+                                           limit=n)
+        df = df.rename(columns={'parentPath':'partition_count'})
+        if n > 0:
+            df = df[:n]
+        self._add_hdfs_subdir_counts(df)
+        return df
+
+    def _add_hdfs_subdir_counts(self, df):
+        dbs = []
+        tables = []
+        for parent_path in df.index:
+            db, table = tuple([x for x in parent_path.split('/')
+                               if len(x) > 0])
+            dbs.append(db)
+            tables.append(table)
+        df['Database'] = dbs
+        df['Table'] = tables
+        df['hdfs_subdir_count'] = self._get_hdfs_subdir_count(dbs, tables)
+        return df.reset_index()
+
+    def _get_hdfs_subdir_count(self, db_names, table_names):
+        counts = []
+        for i in range(0, len(db_names), 30):
+            counts.extend(self._subdir_helper(db_names[i:i+30],
+                                              table_names[i:i+30]))
+        return counts
+
+    def _subdir_helper(self, db_names, table_names):
+        clause_base = '(parentPath:\/{} AND originalName:{})'
+        lst = []
+        for db_name, table_name in zip(db_names, table_names):
+            lst.append(clause_base.format(db_name, table_name))
+        table_query = ('sourceType:HIVE AND type:TABLE AND (' +
+                       ' OR '.join(lst) + ')')
+        tables = self.nav_elements.get_docs(
+            fq=table_query, rows=len(db_names))
+        count_map = {} # (db, tbl) -> count
+        for t in tables:
+            key = (t['parentPath'][1:], t['originalName'])
+            path = _get_file_system_path(t['fileSystemPath'])
+            query = ('sourceType:HDFS AND type:DIRECTORY AND '
+                     '-fileSystemPath:\{0}/*.hive-staging* AND '
+                     'fileSystemPath:\{0}/*').format(path)
+            count_map[key] = self.nav_elements.get_count(query)
+        counts = []
+        for db_name, table_name in zip(db_names, table_names):
+            counts.append(count_map[(db_name, table_name)])
+        return counts
+
+    def _make_deleteTime_query(self, l, u, incl=False, incu=True,
+                               label=None):
+        return frange('deleteTime', l, u, incl, incu, label)
+
+
+def _get_file_system_path(full_path):
+    """
+    hdfs://Enchilada/path -> path
+    """
+    matches = PATH_PATTEN.match(full_path)
+    return matches.group(1)
+
+
+class NavSolrComparator(object):
+
+    def __init__(self, analyzers):
+        self.analyzers = analyzers
+
+    def summary_stats(self):
+        return self._compare('summary_stats')
+
+    def hdfs_stats(self):
+        return self._compare('hdfs_stats').reorder_levels([1,0], 1)
+
+    def count_breakdown(self):
+        return self._compare('count_breakdown').reorder_levels([1,0], 1)
+
+    def deleted_stats(self, fq=None):
+        return self._compare('deleted_stats', fq=fq)
+
+    def create_stats(self, fq=None):
+        return self._compare('create_stats', fq=fq)
+
+    def _compare(self, meth, *args, **kwds):
+        args = list(zip(*[(getattr(a, meth)(*args, **kwds), a.name)
+                          for a in self.analyzers]))
+        return self._concat(args[0], args[1])
+
+    def _concat(self, lst, names):
+        return pd.concat(lst, axis=1, keys=names)
+
+
+
+def to_excel(comparator, path):
+    writer = pd.ExcelWriter(path)
+    comparator.summary_stats().to_excel(writer, 'Summary Stats')
+    comparator.hdfs_stats().to_excel(writer, 'HDFS Stats')
+    comparator.count_breakdown().to_excel(writer, 'Counts Breakdown')
+    comparator.deleted_stats(fq='sourceType:HDFS').to_excel(
+        writer, 'Deleted HDFS Entities')
+    comparator.create_stats(fq='sourceType:HDFS').to_excel(
+        writer, 'Created HDFS Entities')
+    comparator.create_stats(fq='sourceType:HIVE AND type:TABLE').to_excel(
+        writer, 'Created Hive Tables')
+    writer.save()
+    writer.close()
+
+
+
+if __name__=='__main__':
+    import sys
+    if len(sys.argv) < 3:
+        sys.exit(USAGE)
+    conf_file = sys.argv[1]
+    with open(conf_file, 'r') as fh:
+        configs = [l.split(',') for l in fh.readlines()
+                   if not l.startswith('#')]
+    path = sys.argv[2]
+
+    if len(configs) > 1:
+        comp = NavSolrComparator([NavSolrAnalyzer(*conf) for conf in configs])
+    else:
+        comp = NavSolrAnalyzer(*configs[0])
+    to_excel(comp, path)
