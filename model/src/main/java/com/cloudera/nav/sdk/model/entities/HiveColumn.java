2cf5e3e10fcc36d2f7fff52ad171f4a71a8f8e2f
jajodia <jajodia@cloudera.com>
2018-03-16 14:31:09 -0700

    [Custom Hive Column Relations] Fixing bug where relations linking to hive columns was not working

--- model/src/main/java/com/cloudera/nav/sdk/model/entities/HiveColumn.java
+++ model/src/main/java/com/cloudera/nav/sdk/model/entities/HiveColumn.java
@@ -21,6 +21,9 @@ import com.cloudera.nav.sdk.model.HiveIdGenerator;
 import com.cloudera.nav.sdk.model.SourceType;
 import com.cloudera.nav.sdk.model.annotations.MClass;
 import com.cloudera.nav.sdk.model.annotations.MProperty;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 /**
  * Represents a Hive column; uniquely identified by the source id, database name,
@@ -29,6 +32,10 @@ import com.cloudera.nav.sdk.model.annotations.MProperty;
  */
 @MClass(model="hv_column")
 public class HiveColumn extends Entity {
+
+  private final String DATABASE_NAME = "databaseName";
+  private final String TABLE_NAME = "tableName";
+  private final String COLUMN_NAME = "columnName";
 
   @MProperty
   private String databaseName;
@@ -84,5 +91,13 @@ public class HiveColumn extends Entity {
    */
   public void setColumnName(String columnName) {
     this.columnName = columnName;
+  }
+
+  @Override
+  public Map<String, String> getIdAttrsMap() {
+    return ImmutableMap.of(
+        DATABASE_NAME, this.getDatabaseName(),
+        TABLE_NAME, this.getTableName(),
+        COLUMN_NAME, this.getColumnName());
   }
 }
